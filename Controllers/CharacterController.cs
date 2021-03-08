c0dad6648fa2e73f969cc949f28bc09295c099fd
Eduardo Santos <elgsantos@gmail.com>
2021-03-07 21:41:19 -0300

    feat: add Auth to CharacterController

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Models;
 using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class CharacterController : ControllerBase
